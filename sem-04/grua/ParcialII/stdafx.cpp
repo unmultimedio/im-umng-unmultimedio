71c765a7eca80032d4ade8b084efdfc56c1df925
Julián R Figueroa <julianrfigueroa@gmail.com>
2014-01-20 16:18:36 -0500

    Revert "Proyectos Intro Comp Gráfica."
    This reverts commit 731c4bdbefe0c2c156e2b3f508d40f202120b07c.

--- sem-04/grua/ParcialII/stdafx.cpp
+++
@@ -1,7 +0,0 @@
-// stdafx.cpp: archivo de código fuente que contiene solo las inclusiones estándar
-// ParcialII.pch será el encabezado precompilado
-// stdafx.obj contendrá la información de tipos precompilada
-
-#include "stdafx.h"
-
-
